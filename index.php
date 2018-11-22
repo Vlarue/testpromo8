85e33cb420e2d144e20c6bff559b8eac6e46765c
Vlarue <45257447+Vlarue@users.noreply.github.com>
2018-11-22 11:04:40 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test GitHub Desktop</title>
+</head>
+<body>
+	<h1>Mon Titre</h1>
+	<?php 
+		echo date('d/m/Y');
+	 ?>
+	 <p style="color:red;">Bonjour Vincent comment vas-tu?
+</body>
+</html>
